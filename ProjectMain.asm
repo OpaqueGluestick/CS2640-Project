42cff68948b490b9dbe92f65bc31c7e38245dc1a
OpaqueGluestick <gaviemeh@gmail.com>
2026-04-17 15:04:21 -0700

    Add files via upload

---
+++ ProjectMain.asm
@@ -0,0 +1,9 @@
+#Group 3: Anthony Keshishian, Gavin Mah, Muhammad Ebueng, Sebastian Gutierrez
+#4/13/2026
+#Blackjack Project
+
+.include "macros.asm"
+
+.data
+
+.text
---
+++ macros.asm
@@ -0,0 +1,25 @@
+#macros for the project
+.data
+	nL: .asciiz "\n"
+.text
+
+.macro printString(%string)
+	li $v0, 4
+	la $a0, %string
+	syscall
+.end_macro 
+
+.macro printInt(%Int)
+	li $v0, 1
+	la $a0, %Int
+	syscall
+.end_macro 
+
+.macro exitProgram
+	li $v0, 10
+	syscall
+.end_macro 
+
+.macro newLine
+	printString(nL)
+.end_macro 
