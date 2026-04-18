c97f8defdeee6d53b451d770be3ea3935a29a9e4
OpaqueGluestick <gaviemeh@gmail.com>
2026-04-17 18:20:58 -0700

    Add files via upload

--- ProjectMain.asm
+++ ProjectMain.asm
@@ -3,7 +3,18 @@
 #Blackjack Project
 
 .include "macros.asm"
+.eqv $t0, currentMoney
+.eqv $t1, betAmount
+.eqv $t2, dealerHiddenCardValue
 
 .data
-
+	
 .text
+dealCard:
+	#Random Number Generator (a0=seed, a1=upper limit not inclusive, output to a0)
+	li $v0, 42 
+	li $a0, 0
+	li $a1, 53
+	move $t2 ,$a0
+	
+	
