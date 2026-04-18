0d76197337be26c2fc651523826bc97914931b4c
OpaqueGluestick <gaviemeh@gmail.com>
2026-04-17 18:52:23 -0700

    Add files via upload

--- ProjectMain.asm
+++ ProjectMain.asm
@@ -13,16 +13,17 @@
 .eqv playerHasAce $t6
 
 .data
-	
+baseDeck: .word a, a, a, a, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, J, J, J, J, Q, Q, Q, Q, K, K, K, K
+
 .text
 dealCard:
-	#Random Number Generator (puts a number from 0-51 into $t2)
+	#Random Number Generator (puts a number from 0-51 into $a0)
 	li $v0, 42 
 	li $a0, 0
 	li $a1, 52
 	syscall
 	#move $t5 ,$a0  (may change)
-	
+
 	printInt($t2)
 	
 	exitProgram
