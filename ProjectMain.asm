23b2e2376b39422b7e6640ace7dd5d6f7a52c778
OpaqueGluestick <gaviemeh@gmail.com>
2026-04-19 22:59:38 -0700

    Basic Game function

--- ProjectMain.asm
+++ ProjectMain.asm
@@ -16,6 +16,17 @@
 .data
 helloplayerMsg: .asciiz "Hello player, welcome to our Blackjack simulation\n"
 wagerMsg: .asciiz "How much would you like to wager? \n"
+choicePrompt: .asciiz "Hit or Stand? (Enter 'H' or 'S') "
+dHand: .asciiz "Dealer Hand: "
+questionmark: .asciiz "?"
+plusSign: .asciiz " + "
+pHand: .asciiz "Your Hand Value: "
+impBuffer: .space 5
+bustMessage: .asciiz "Bust"
+pWinMessage: .asciiz "You Win"
+dWinMessage: .asciiz "Dealer Wins"
+tiedMessage: .asciiz "Its a Tie"
+arrow: .asciiz "->"
 
 .text
 #Print the hello message
@@ -42,4 +53,82 @@ startGame:
 	#dealCard(playerHandValue, playerHasAce)
 	#printInt(playerHandValue)
 	
+newHand:
+	#Deal Starting Hands
+	printString(dHand)#Dealer Hand Value: # + ?
+	dealCard(dealerHandValue, dealerHasAce)
+	dealCard(dealerHiddenCardValue, dealerHasAce)
+	printInt(dealerHandValue)
+	printString(plusSign)
+	printString(questionmark)
+	newLine
+	
+	printString(pHand)
+	dealCard(playerHandValue, playerHasAce)
+	printInt(playerHandValue)
+	printString(arrow)
+	dealCard(playerHandValue, playerHasAce)
+	#TODO: make it show 1st card + 2nd card instead of total hand value (currently shows "1st card" + "(1st + 2nd)"
+	printInt(playerHandValue)
+	newLine
+	
+decisionLoop:
+	printString(choicePrompt)
+	li $v0, 8
+	la $a0, impBuffer
+	li $a1, 5
+	syscall
+	
+	#check input
+	la $s2, impBuffer
+	lb $s2, 0($s2)
+	li $s3, 'H'
+	beq $s2, $s3, hit
+stand:
+	#remind player of their hand
+	printString(pHand)
+	printInt(playerHandValue)
+	newLine
+	
+	#reveal hand's value
+	printString(dHand)
+	add dealerHandValue, dealerHandValue, dealerHiddenCardValue
+	printInt(dealerHandValue)
+	newLine
+	
+	#dealer hand < 16 = hit; else stand (subject to change)
+dealerHitLoop:
+	bge dealerHandValue, 17, checkWinner
+	printString(dHand)
+	dealCard(dealerHandValue, dealerHasAce)
+	printInt(dealerHandValue)
+	newLine
+	
+	j dealerHitLoop
+	
+checkWinner:
+	bge dealerHandValue, 22, playerWin
+	beq dealerHandValue, playerHandValue, tieGame
+	bgt dealerHandValue, playerHandValue, dealerWin
+	#if not above then player hand > dealer hand and goes to player win 
+playerWin:
+	printString(pWinMessage)
+	j exit
+dealerWin:
+	printString(dWinMessage)
+	j exit
+tieGame:
+	printString(tiedMessage)
+	j exit
+	
+hit:
+	printString(pHand)
+	dealCard(playerHandValue, playerHasAce)
+	printInt(playerHandValue)
+	newLine
+	ble playerHandValue, 21, decisionLoop
+	#only happens if over 21
+	printString(bustMessage)
+#TODO: Make it loop back to startGame with a player choice
+exit:
 	exitProgram
