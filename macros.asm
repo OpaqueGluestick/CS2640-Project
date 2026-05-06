e409342227e8f696e432f94b4b3d15f30bdd7da6
OpaqueGluestick <gaviemeh@gmail.com>
2026-05-05 23:54:36 -0700

    Fixed deck size added

--- macros.asm
+++ macros.asm
@@ -2,11 +2,15 @@
 .data
 	nL: .asciiz "\n"
 	divides: .asciiz "--------------------------------\n"
-	baseDeck: .word 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13
 	A: .asciiz "Ace"
 	J: .asciiz "Jack"
 	Q: .asciiz "Queen"
 	K: .asciiz "King"
+	#full deck never gets edited while base deck gets cards removed from it
+	baseDeckSize: .word 52
+	fullDeck: .word 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13
+	baseDeck: .word 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12, 12, 12, 13, 13, 13, 13
+	
 .text
 
 .macro printString(%string)
@@ -46,20 +50,63 @@
 	newLine
 .end_macro
 
+#TODO t7 t9
+
+.macro refillDeck #only as macro to look better/less clutter in dealCard
+refillLoop:	
+	beq $t9, 52, endRefill 
+	lw $s1, ($t7) 
+	sw $s1, ($s0) #takes and puts number from full deck into baseDeck
+	add $s0, $s0, 4
+	add $t7, $t7, 4
+	add $t9, $t9, 1
+	j refillLoop
+endRefill:
+.end_macro 
+
+
 .macro dealCard(%personHandValue,%aceRegister)
 	#adds drawn card's value to person's hand value and updates ace
+	la $t9, baseDeckSize
+	lw $t9, ($t9) 
+	#get the current deck's size; skip over refill deck section if deck size isnt 0
+	bgtz $t9, drawCard 
 	
-	#Random Number Generator (puts a number from 0-51 into $a0)
+	#load addresses for refillDeck loop; $t9 becomes a counter
+	la $s0, baseDeck
+	la $t7, fullDeck
+	refillDeck
+	
+	#after refill, set baseDeckSize back to 52
+	la $s0, baseDeckSize
+	sw $t9, ($s0) 
+drawCard:
+	#Random Number Generator (puts a number from 0 to basedecksize-1 into $a0)
 	li $v0, 42 
 	li $a0, 0
-	li $a1, 52
+	lw $a1, baseDeckSize 	
 	syscall
 	
-	#change RN into deck's card (ex 50->13->K)
+	#change RN into deck's card (ex 50->13->K) and removes the card from deck
 	la $s0, baseDeck
+	la $t9, baseDeckSize
+	lw $t9, ($t9) #gets the deck's size
+	sub $t9, $t9, 1 #last value is n-1 where n is number of cards in deck 
+	mul $t9, $t9, 4 
+	add $s0, $s0, $t9
+	lw $t9, ($s0)  #sets t9 to the last value of baseDeck
+	
+	la $s0, baseDeck #reset address
 	mul $a0, $a0, 4
 	add $s0, $s0, $a0
-	lw $s1, 0($s0)
+	lw $s1, 0($s0) #set $s1 to value of drawn card
+	
+	
+	sw $t9, ($s0) #overwrites the drawn card with the value of the last
+	la $t9, baseDeckSize
+	lw $t7, ($t9)
+	sub $t7, $t7, 1
+	sw $t7, ($t9) #decrease baseDeckSize
 	
 	#branches for non number cards (ace,jack,queen,king)
 	beq $s1, 1, drawAce
